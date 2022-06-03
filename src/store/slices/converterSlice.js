73ab1a91a498782d9f275a2af0b4384171f9016f
Mysik <Mishamysik@gmail.com>
2022-06-03 11:47:44 +0300

    create store

--- src/store/slices/converterSlice.js
+++ src/store/slices/converterSlice.js
@@ -1,4 +1,15 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import {API_URL, KEY} from "../../data/data";
+const loadCurrentCourse = createAsyncThunk(
+    'converter/load',
+    async (f,s) => {
+        try {
+            const response = await fetch(`${API_URL}?apiKey=${KEY}&q=${f}_${s}&compact=ultra`);
+        } catch (e) {
+            alert(e.message);
+        }
+    }
+);
 
 const converterSlice = createSlice({
     name: 'converter',
@@ -6,8 +17,24 @@ const converterSlice = createSlice({
         leftOperand: null,
         rightOperand: null,
         course: null,
+        leftSelect: null,
+        rightSelect: null,
     },
-
+    reducers: {
+      changeLeftOperand: (state, action) => {
+          state.leftOperand = action.payload;
+          state.rightOperand = state.leftOperand * state.course;
+      },
+      changeRightOperand: (state, action) => {
+          state.rightOperand = action.payload;
+          state.leftOperand = state.course/state.rightOperand;
+      },
+    },
+    extraReducers: {
+        [loadCurrentCourse.fulfilled]: (state, action) => {
+            state.course = action.payload
+        }
+    }
 })
 
 export default converterSlice.reducer;
