67e633d61e7e439ac410aff5ff5c8704ab5e2327
Mysik <Mishamysik@gmail.com>
2022-06-03 11:06:19 +0300

    create select

--- src/components/ConversionSelect/ConversionSelect.jsx
+++ src/components/ConversionSelect/ConversionSelect.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './conversion-select.scss';
+import {currencyList} from "../../data/data";
 
 const ConversionSelect = () => {
+
+    const [baseCurrency, setBaseCurrency] = useState('USD');
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const dropdownHandler = () => setIsDropdownOpen(!isDropdownOpen);
+    const pickCurrency = (item) => {
+        setIsDropdownOpen(false);
+        setBaseCurrency(item);
+    }
     return (
-        <div>
-            ConversionSelect
+        <div className='conversion-select'>
+            <h6 className='conversion-select--title' onClick={dropdownHandler}>
+                {baseCurrency}
+            </h6>
+            <ul className={`conversion-select--dropdown ${isDropdownOpen && 'conversion-select--dropdown__open'}`}>
+                {
+                    currencyList.map((el) => <li className='conversion-select--item' key={el.id} onClick={() => pickCurrency(el.currency)}>{el.currency}</li>)
+                }
+            </ul>
         </div>
     );
 };
--- src/data/data.js
+++ src/data/data.js
@@ -1,2 +1,8 @@
 export const KEY = 'cd2ff852330c08894153';
 export const API_URL = 'https://free.currconv.com/api/v7/convert';
+
+export const currencyList = [
+    {id: 1, currency:'UAH'},
+    {id: 2, currency:'USD'},
+    {id: 3, currency:'EUR'},
+]
