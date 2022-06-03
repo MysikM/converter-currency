b5ebb4aff641fdd893a42f551168e3b7556e6cbc
Mysik <Mishamysik@gmail.com>
2022-06-03 10:19:17 +0300

    add currency exchange title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from "./components/Header/Header";
+import CurrencyExchangeTitle from "./components/CurrencyExchangeTitle/CurrencyExchangeTitle";
 
 function App() {
   return (
     <div className='container'>
         <Header />
+        <CurrencyExchangeTitle />
     </div>
   );
 }
---
+++ src/components/CurrencyExchangeTitle/CurrencyExchangeTitle.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './currency-exchange-title.scss';
+
+const CurrencyExchangeTitle = () => {
+    return (
+        <section className='live-exchange'>
+            <h2 className='live-exchange--title'>1 USD = 29 UAH.</h2>
+        </section>
+    );
+};
+
+export default CurrencyExchangeTitle;
